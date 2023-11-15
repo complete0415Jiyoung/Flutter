ae2e35e43f83822b44c42c2df4d623254b4cc8dd
LJY <complete0415@naver.com>
2023-11-16 00:28:10 +0900

    인스타그램_좋아요 기능, 파일 분리하기

--- instergram/lib/main.dart
+++ instergram/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'home_page.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -16,43 +18,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(CupertinoIcons.camera, color: Colors.black),
-          onPressed: () {},
-        ),
-        actions: [
-          IconButton(
-            icon: Icon(CupertinoIcons.paperplane, color: Colors.black),
-            onPressed: () {},
-          )
-        ],
-        title: Image.asset(
-          'assets/logo.png',
-          height: 32,
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.white,
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // 이미지
-          Image.network(
-            "https://i.ibb.co/YjjLCS7/cat.png",
-            height: 400,
-            width: double.infinity,
-            fit: BoxFit.cover,
-          ),
-        ],
-      ),
-    );
-  }
-} // StatelessWidget
